9b4318a97e22fe78c6485d7786d2764dfb114743
Javier Arenas Iglesias <javierareigl@gmail.com>
2017-07-19 19:37:18 +0200

    now it shows the artist image, album image, and the album title in index.html (alpha3)

--- web-api-auth-examples/authorization_code/public/js/spotify.js
+++ web-api-auth-examples/authorization_code/public/js/spotify.js
@@ -1,4 +1,4 @@
-var obj;
+var obj2;
 
 (function($) {
 
@@ -34,23 +34,61 @@ $.ajax({
    dataType: 'json',
    data: {
      	type: "artist",
+      	query : $textBoxArtist.val().toString()
+    },
+   headers: {
+       'Authorization': 'Bearer ' + access_token
+   },
+   success: function(response) {
+   		getArtists(response);
+   }
+});
+$.ajax({
+   url: 'https://api.spotify.com/v1/search',
+   dataType: 'json',
+   data: {
+     	type: "album",
       	query : $textBoxArtist.val()
     },
    headers: {
        'Authorization': 'Bearer ' + access_token
    },
-   success: function(response) {   	
-   		getArtists(response);
-   		obj = response;
+   success: function(response2) {  
+   		getAlbum(response2);
    }
 });
 });
 
 
+
 })(jQuery);
 
 function getArtists(response) {
-	var nameArtist = response.artists.items[0].name;
-	var img = 
-	document.getElementById('result').innerText = nameArtist;
+
+	var nameArtist = [];
+  	var items =response.artists.items;
+  	$('#result').empty();
+   	for (var i=0; i<items.length; i++ ){
+   		nameArtist[i] = items[i].name;
+    	$('#result').append("<p>" + nameArtist[i] + "</p>");
+    	var imageArtist = items[i].images[0].url; //inicializa cogiendo la primera imagen del objeto de artista
+		var img = document.createElement("img"); // crea el elemento img
+		document.getElementById('result').appendChild(img).setAttribute("class", "imgArtist"); //añade el id de imgArtist
+		document.getElementById('result').appendChild(img).setAttribute("src", imageArtist); //mete el source de la imagen 
+ 	};
+	
+}
+
+function getAlbum(response2){
+	var albumResult = document.getElementById('resultImg');
+	var imgClass = document.getElementsByClassName('albumImg');
+	var img = document.createElement("img"); // crea el elemento img
+	for(var i = 0; i < response2.albums.items.length; i++){
+		var imgItem = response2.albums.items[i];
+		var imgName = document.createElement("p");
+		img = document.createElement("img");
+		albumResult.appendChild(img).setAttribute("class", "albumImg"); //añade el id de imgArtist
+		albumResult.appendChild(img).setAttribute("src", imgItem.images[1].url);
+		$("<h2>" + imgItem.name + "</h2>").insertAfter(imgClass[i]);
+	}	
 }
